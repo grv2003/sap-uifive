ecbb59b062db451a7cd16028722098a2a26245f6
Gaurav Arora <gaurav.arora@mannlowe.com>
2021-04-27 18:11:55 +0530

    module

--- webapp/index.js
+++ webapp/index.js
@@ -6,3 +6,8 @@ sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
     oView.placeAt("content");
   });
 });
+
+//Use sap.ui.define to define a new module.
+//sap.ui.define([dependency library], function(doSomething))
+//With "use strict":
+//Using a variable without declaring it, is not allowed.
