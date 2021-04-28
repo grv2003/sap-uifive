84487044cd5faf755691a74194044bf90bafd836
Gaurav Arora <gaurav.arora@mannlowe.com>
2021-04-28 16:34:23 +0530

    Component Config

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,16 @@
-sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
-  "use strict";
-  XMLView.create({
-    viewName: "sap.ui.demo.walkthrough.view.App",
-  }).then(function (oView) {
-    oView.placeAt("content");
-  });
-});
+sap.ui.define(
+  ["sap/ui/core/ComponentContainer"],
+  function (ComponentContainer) {
+    "use strict";
+    new ComponentContainer({
+      name: "sap.ui.demo.walkthrough",
+      settings: {
+        id: "walkthrough",
+      },
+      async: true,
+    }).placeAt("content");
+  }
+);
 
 //Use sap.ui.define to define a new module.
 //sap.ui.define([dependency library], function(doSomething))
