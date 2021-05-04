6d74726e6f5545e698267c53f48bece0d7863f69
Gaurav Arora <gaurav.arora@mannlowe.com>
2021-05-04 16:23:12 +0530

    data types

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define(
   ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
-  function (Controller, MessageToast, Fragement) {
+  function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
       onShowHello: function () {
